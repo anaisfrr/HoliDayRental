80f0c60db96ea065999cc24fe0c9bf97553cd456
Anais Francois <anaisfrancois8@gmail.com>
2022-02-22 22:09:43 +0100

    commit suite dal

--- HoliDayRental/Repositories/OptionsBienService.cs
+++ HoliDayRental/Repositories/OptionsBienService.cs
@@ -10,17 +10,48 @@ namespace HoliDayRental.DAL.Repositories
     {
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM [OptionsBien] WHERE [Id] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
         public OptionsBien Get(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idOption],[idBien],[Valeur] FROM [OptionsBien] WHERE [idOption] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read()) return Mapper.ToOptionsBien(reader);
+                    return null;
+                }
+            }
         }
 
         public IEnumerable<OptionsBien> Get()
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idOption],[Libelle],[idBien] FROM [OptionsBien]";
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read()) yield return Mapper.ToOptionsBien(reader);
+                }
+            }
         }
 
         public IEnumerable<OptionsBien> GetByBienEchange(int idBien)
@@ -30,12 +61,40 @@ namespace HoliDayRental.DAL.Repositories
 
         public int Insert(OptionsBien entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO [OptionsBien]([idOption], [idBien], [Valeur] OUTPUT [inserted].[Id] VALUES (@idOption, @idBien, @valeur)";
+                    SqlParameter p_idOption = new SqlParameter { ParameterName = "idOption", Value = entity.idOption };
+                    SqlParameter p_idBien = new SqlParameter { ParameterName = "idBien", Value = entity.idBien };
+                    SqlParameter p_valeur = new SqlParameter { ParameterName = "valeur", Value = entity.Valeur };
+                    command.Parameters.Add(p_idOption);
+                    command.Parameters.Add(p_idBien);
+                    command.Parameters.Add(p_valeur);
+                    connection.Open();
+                    return (int)command.ExecuteScalar();
+                }
+            }
         }
 
         public void Update(int id, OptionsBien entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE [OptionsBien] SET [idOption] = @idOption, [idBien] = @idBien, [Valeur] = @valeur WHERE [Id] = @id";
+                    SqlParameter p_idOption = new SqlParameter { ParameterName = "idOption", Value = entity.idOption };
+                    SqlParameter p_idBien = new SqlParameter { ParameterName = "idBien", Value = entity.idBien };
+                    SqlParameter p_valeur = new SqlParameter { ParameterName = "valeur", Value = entity.Valeur };
+                    command.Parameters.Add(p_idOption);
+                    command.Parameters.Add(p_idBien);
+                    command.Parameters.Add(p_valeur);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
--- HoliDayRental/Repositories/OptionsService.cs
+++ HoliDayRental/Repositories/OptionsService.cs
@@ -10,17 +10,48 @@ namespace HoliDayRental.DAL.Repositories
     {
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM [Options] WHERE [Id] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
         public Options Get(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idOption],[Libelle] FROM [Options] WHERE [idOption] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read()) return Mapper.ToOptions(reader);
+                    return null;
+                }
+            }
         }
 
         public IEnumerable<Options> Get()
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idOption],[Libelle] FROM [Options]";
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read()) yield return Mapper.ToOptions(reader);
+                }
+            }
         }
 
         public IEnumerable<Options> GetByBienEchange(int idBien)
@@ -30,12 +61,36 @@ namespace HoliDayRental.DAL.Repositories
 
         public int Insert(Options entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO [Options]([idOptions], [Libelle] OUTPUT [inserted].[Id] VALUES (@idOption, @libelle)";
+                    SqlParameter p_idOption = new SqlParameter { ParameterName = "idOption", Value = entity.idOption };
+                    SqlParameter p_libelle = new SqlParameter { ParameterName = "libelle", Value = entity.Libelle };
+                    command.Parameters.Add(p_idOption);
+                    command.Parameters.Add(p_libelle);
+                    connection.Open();
+                    return (int)command.ExecuteScalar();
+                }
+            }
         }
 
         public void Update(int id, Options entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE [Options] SET [idOption] = @idOption, [Libelle] = @libelle WHERE [Id] = @id";
+                    SqlParameter p_idOption = new SqlParameter { ParameterName = "idOption", Value = entity.idOption };
+                    SqlParameter p_libelle = new SqlParameter { ParameterName = "libelle", Value = entity.Libelle };
+                    command.Parameters.Add(p_idOption);
+                    command.Parameters.Add(p_libelle);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
