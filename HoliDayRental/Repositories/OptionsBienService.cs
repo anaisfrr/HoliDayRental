8adb851e6934b8aa34091cc122f831e2ee93c286
Anais Francois <a.francois@interface3.be>
2022-02-23 09:33:46 +0100

    commit suite dal

--- HoliDayRental/Repositories/OptionsBienService.cs
+++ HoliDayRental/Repositories/OptionsBienService.cs
@@ -10,17 +10,48 @@ namespace HoliDayRental.DAL.Repositories
     {
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM [OptionsBien] WHERE [Id] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
         public OptionsBien Get(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idOption],[idBien],[Valeur] FROM [OptionsBien] WHERE [idOption] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read()) return Mapper.ToOptionsBien(reader);
+                    return null;
+                }
+            }
         }
 
         public IEnumerable<OptionsBien> Get()
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idOption],[Libelle],[idBien] FROM [OptionsBien]";
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read()) yield return Mapper.ToOptionsBien(reader);
+                }
+            }
         }
 
         public IEnumerable<OptionsBien> GetByBienEchange(int idBien)
